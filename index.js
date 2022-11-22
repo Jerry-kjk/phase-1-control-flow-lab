7ab45385cddfc6f1a7877be6c37de3cde11c1e5d
jerry-kjk <jerrykajik@gmail.com>
2022-11-22 23:35:51 +0300

    Added a simple title

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let ride;
+  if ( feet <=400){
+    ride='This one is on me!';
+  }else if(feet> 2000 && feet < 2500){
+    ride = 'I will gladly take your thirty bucks.';
+  }else if(feet > 2500){
+    ride = 'No can do.'
+  }
+  return ride;
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+scuberGreetingForFeet(199);
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(cityName){
   // Write your code here!
-}
+  //syntax = condition ? true : false
+  let city;
+  return city = cityName === 'NYC' ? ("Ok, sounds good.") : ("No g
